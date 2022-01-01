439f406d2d1a1eee4da65f28f0506c1e0fa3c890
Gaurav <gauravjain0931@gmail.com>
2022-01-01 16:52:02 +0530

    git

--- Basic/src/app/app.component.ts
+++ Basic/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Basic';
+export class AppComponent implements OnChanges{
+  title: number;
+  status: any = [];
+  Item: any = [];
+
+  constructor()
+  {
+    this.Item = ["SUBMITTED",'RECEIVED','DULY-FILED','GRANTED'];
+    this.status = this.Item;
+    console.log(this.status[0]);
+
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+
+  }
+  gotoCLick(event: any)
+  {
+    console.log(event);
+  }
+
 }
+
+
--- Basic/src/app/app.module.ts
+++ Basic/src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { QuillModule } from 'ngx-quill';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import 'hammerjs';
+import {MatButtonModule} from '@angular/material/button'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolTipListPipe } from './tool-tip-list.pipe';
+import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolTipListPipe,
+    ProgressBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    QuillModule.forRoot(),
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
